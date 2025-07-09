56279e59aa959bc1c4ee201a76a2a29fcaa39d89
Dikocesrt <dikocesrt@gmail.com>
2025-07-09 13:10:31 +0700

    feat: added function to replace word in sentence with root words

--- soal_a/soal_a.go
+++ soal_a/soal_a.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"sort"
+	"strings"
+)
+
+func replaceWithRootWords(rootWords []string, sentence string) string {
+	temp := make([]string, len(rootWords))
+
+	copy(temp, rootWords)
+	sort.Slice(temp, func(i, j int) bool {
+		return len(temp[i]) < len(temp[j])
+	})
+
+	listWords := strings.Split(sentence, " ")
+	for idx, word := range listWords {
+		replaced := false
+		for _, root := range temp {
+			if strings.HasPrefix(word, root) {
+				listWords[idx] = root
+				replaced = true
+				break
+			}
+		}
+		if !replaced {
+			listWords[idx] = word
+		}
+	}
+	return strings.Join(listWords, " ")
+}
+
 func main(){
 
 }
