ac2e693d43186d7742b13dfd93546f932e874158
Dikocesrt <dikocesrt@gmail.com>
2025-07-09 20:12:12 +0700

    feat: added json for test case

--- soal_a/soal_a.go
+++ soal_a/soal_a.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
 	"sort"
 	"strings"
 )
+
+type TestCase struct {
+    ID        int      `json:"id"`
+    RootWords []string `json:"root_words"`
+    Sentence  string   `json:"sentence"`
+}
+
+type TestData struct {
+    TestCases []TestCase `json:"test_cases"`
+}
 
 func replaceWithRootWords(rootWords []string, sentence string) string {
 	temp := make([]string, len(rootWords))
@@ -31,6 +44,23 @@ func replaceWithRootWords(rootWords []string, sentence string) string {
 	return strings.Join(listWords, " ")
 }
 
+func loadTestData(filename string) (*TestData, error) {
+    file, err := os.Open(filename)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    var testData TestData
+    decoder := json.NewDecoder(file)
+    err = decoder.Decode(&testData)
+    if err != nil {
+        return nil, err
+    }
+
+    return &testData, nil
+}
+
 func main(){
 	// Soal 1
 	rootWords1 := []string{"cat", "bat", "rat"}
@@ -46,4 +76,22 @@ func main(){
 	result2 := replaceWithRootWords(rootWords2, sentence2)
 	fmt.Println("Soal 2 Output:")
 	fmt.Println(result2)
+
+	testData, err := loadTestData(filepath.Join("soal_a", "test.json"))
+    if err != nil {
+        fmt.Printf("Error loading test data: %v\n", err)
+        return
+    }
+
+	// Additional test cases from JSON
+	fmt.Println(strings.Repeat("-", 50))
+    for _, testCase := range testData.TestCases {
+        fmt.Printf("\nTest Case %d:\n", testCase.ID)
+        fmt.Printf("Root Words: %v\n", testCase.RootWords)
+        fmt.Printf("Original Sentence: %s\n", testCase.Sentence)
+        
+        result := replaceWithRootWords(testCase.RootWords, testCase.Sentence)
+        fmt.Printf("Result: %s\n", result)
+        fmt.Println(strings.Repeat("-", 50))
+    }
 }
